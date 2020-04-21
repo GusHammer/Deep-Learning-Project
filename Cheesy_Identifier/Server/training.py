5198ba07164861b2e750e70d2f5d7b4238480fed
andrewlod <39911925+andrewlod@users.noreply.github.com>
2020-04-21 09:37:36 -0300

    removed invalid camembert images

--- Cheesy_Identifier/Server/training.py
+++ Cheesy_Identifier/Server/training.py
@@ -1,8 +1,10 @@
-import tensorflow as tf
-from tensorflow import keras
+#import tensorflow as tf
+#from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
-classes = ["Mozzarella", "Parmesan", "Cheddar", "Gouda", "Swiss", "Camembert", "Feta", "Provolone", "Edam", "Gorgonzola", "Ricotta", "Cottage"]
+classes = ["Mozzarella", "Parmesan", "Cheddar", "Gouda", 
+	"Swiss", "Camembert", "Feta", "Provolone", 
+	"Edam", "Gorgonzola", "Ricotta", "Cottage"]
 
 #todo: import images and labels from database, create model and layers and train it

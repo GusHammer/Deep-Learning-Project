0bcf9170cc14f930521ed85825c31ea82fc116be
andrewlod <39911925+andrewlod@users.noreply.github.com>
2020-05-01 14:04:25 -0300

    client-server early communication implemented

---
+++ Cheesy_Identifier/Server/requesting.js
@@ -0,0 +1,16 @@
+function submit(){
+	console.log("wjwjw");
+	var data = new FormData();
+	data.append('file', document.getElementById("file").files[0]);
+	$.ajax({
+		url: 'http://localhost:5000/api/v1/predict/sample',
+		type: 'POST',
+		contentType: false,
+		cache: false,
+		processData: false,
+		data: data,
+		success: function (data) {
+			console.log(data)
+		}
+	});
+}
--- Cheesy_Identifier/Server/testing.py
+++ Cheesy_Identifier/Server/testing.py
@@ -1,10 +1,17 @@
+#!flask/bin/python
+from flask import Flask, jsonify
+from flask import request
+from flask_cors import CORS
 import tensorflow as tf
 import os
 from tensorflow import keras
 from PIL import Image
+import io
 import numpy as np
+import matplotlib.pyplot as plt
 
-model = keras.models.load_model("model")
+app = Flask(__name__)
+CORS(app)
 
 path = os.getcwd()
 
@@ -20,17 +27,28 @@ for directory in os.listdir():
 os.chdir(path)
 
 test = Image.open("cam.png")
-test = test.resize((128,128))
-test = np.array(test)
-if test.shape[2] == 4:
-	test = test[:, :, :3]
-predictions = model.predict([[test]])
-highest = 0
-value = 0
-print(predictions)
-for item in range(len(predictions[0])):
-	if predictions[0][item] > value:
-		highest = item
-		value = predictions[0][item]
-		
-print(classes[int(highest)])
+
+@app.route('/api/v1/predict/sample', methods=['POST'])
+def predict():
+	#if not request.form:
+	#	return "400"
+	img = request.files.get('file', '')
+	test = Image.open(img)
+	test = test.resize((128,128))
+	test = np.array(test)
+	if test.shape[2] == 4:
+		test = test[:, :, :3]
+	model = keras.models.load_model("model")
+	predictions = model.predict([[test]])
+	highest = 0
+	value = 0
+	for item in range(len(predictions[0])):
+		if predictions[0][item] > value:
+			highest = item
+			value = predictions[0][item]
+	
+	return classes[int(highest)]
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
